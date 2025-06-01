fe90ae4a7a84a2b616e0b39385872cf5e6eb324c
Nitin Kumar Dey <105306697+nkdey14@users.noreply.github.com>
2025-06-02 00:20:37 +0530

    Update HelloWorldController.java

--- src/main/java/com/demo_app/controller/HelloWorldController.java
+++ src/main/java/com/demo_app/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-	@GetMapping("/")
+	@GetMapping("/message")
 	public String getMessage() {
 		
 		return "Hey, Glad to See You!!<br><br>I'm Nitin and I'll be assisting you with your queries.";
